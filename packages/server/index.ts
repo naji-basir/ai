dc384102386b2c81c4b7397963e6db771f7e9bf0
Basir Naji <basirnaji007@gmail.com>
2026-02-17 23:49:05 +0430

    first codes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3008;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is runnig on http://localhost:${port}`);
+});
