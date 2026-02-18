71a6a91bd2c9a8731d3369b9f91a0b3c02f139ff
Basir Naji <basirnaji007@gmail.com>
2026-02-18 23:51:51 +0430

    first api call and receiving data from backend.

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
