808241c56cfec50eabd83825ff272b79227dbf45
Basir Naji <basirnaji007@gmail.com>
2026-02-20 19:33:52 +0430

    MVC

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,55 +1,22 @@
-import type { Request, Response } from "express";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Ollama } from "ollama";
+import router from "./src/routes/aiChatBot.route";
 
 dotenv.config();
-
-const client = new Ollama({
-  host: "https://ollama.com",
-  headers: { Authorization: "Bearer " + process.env.OLLAMA_API_KEY },
-});
-
 const port = process.env.PORT || 3008;
 const app = express();
+
+// middlewares
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+app.use((req, res, next) => {
+  console.log("Hello from middleware...");
+  next();
 });
 
-app.post("/api/chat", async (req: Request, res: Response) => {
-  try {
-    const { prompt } = req.body as { prompt: string };
-
-    if (!prompt) {
-      return res.status(400).json({ error: "Prompt is required" });
-    }
-
-    const response = await client.chat({
-      model: "gpt-oss:120b",
-      messages: [{ role: "user", content: prompt }],
-      options: {
-        temperature: 0.4,
-      },
-    });
-
-    console.log(response);
-
-    res.json({
-      reply: response?.message?.content ?? "No response returned",
-    });
-  } catch (error: any) {
-    console.error("Ollama error:", error?.response?.data || error.message);
-    res.status(500).json({ error: "API request failed" });
-  }
-});
-
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.json({ message: "Hello, World" });
-});
+// routes
+app.use("/api/chat", router);
 
 app.listen(port, () => {
   console.log(`Server is runnig on http://localhost:${port}`);
